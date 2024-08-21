c751b55f903636bbf7545f75e3cfd4e49d0d135d
Lander Vitug <lander.vitug@gmail.com>
2024-08-21 12:24:22 -0500

    Create the project manager

---
+++ src/js/project-manager.js
@@ -0,0 +1,33 @@
+import { Project } from "js/models/projects.js";
+
+class _ProjectManager {
+  constructor() {
+    this._projects = [];
+  }
+
+  createProject(name, color) {
+    const newProject = new Project(name, color);
+    this._projects.push(newProject);
+  }
+
+  findProject(projectId) {
+    return this._projects.find(project => project.id === projectId);
+  }
+
+  updateProject(projectId, newName, newColor) {
+    const project = findProject(projectId);
+    project.name = newName;
+    project.newColor = newColor;
+  }
+
+  removeProject(projectId) {
+    this._projects = this._projects.filter(project => project.id !== projectId);
+  }
+
+  getProjects() {
+    return this._projects;
+  }
+}
+
+const ProjectManager = new _ProjectManager();
+export { ProjectManager };
