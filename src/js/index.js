a53503500d48efcdb3ff9700a30b7165dc993787
lander-vitug-digy4 <lander.vitug@digy4.com>
2024-09-24 14:55:22 -0500

    Refactor styles, html, and media

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,65 @@ import "styles/reset.css";
 import "styles/layout.css";
 import "styles/sidebar.css";
 import "styles/content-area.css";
+
+/*
+
+<div class="task-item">
+  <div class="task-content">
+    <img
+      src="<%= require('media/checkbox-blank-outline.svg') %>"
+      alt="checkbox icon"
+      class="task-checkbox"
+    />
+    <p class="task-title">Buy Eggs</p>
+    <img
+      src="<%= require('media/delete-outline.svg') %>"
+      alt="delete icon"
+      class="task-delete-icon"
+    />
+  </div>
+  <div class="task-options-group">
+    <div class="task-priority">
+      <label for="task-priority-select">Priority:</label>
+      <select
+        id="task-priority-select"
+        name="priority"
+        class="task-select"
+      >
+        <option value="none">None</option>
+      </select>
+    </div>
+
+    <div class="task-date">
+      <label for="task-date-input">Date:</label>
+      <input
+        id="task-date-input"
+        type="date"
+        name="date"
+        class="task-date-input"
+      />
+    </div>
+
+    <div class="task-project">
+      <label for="task-project-select">Project:</label>
+      <select
+        id="task-project-select"
+        name="project"
+        class="task-select"
+      >
+        <option value="none">None</option>
+      </select>
+    </div>
+  </div>
+</div>
+
+*/
+
+/*
+
+<div class="section-item project-item">
+  <span class="project-indicator"></span>
+  <span class="project-name">INBOX</span>
+</div>
+
+*/
